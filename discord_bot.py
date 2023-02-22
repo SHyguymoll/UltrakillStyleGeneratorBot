ef221c152183028daaf709527c899e2b05bd714d
SHyguymoll <shymollet@gmail.com>
2023-02-21 23:39:45 -0800

    fix

--- discord_bot.py
+++ discord_bot.py
@@ -9,8 +9,8 @@ client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
 @tree.command(name = "generate_text", description = "Characters Supported: a-Z, +, -, (, )") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_command(interaction, string:str, color:generate_image.TColor):
-    generate_image.generate_image(string, color).save(string)
+async def first_command(interaction, string:str, color:str):
+    generate_image.generate_image(string, generate_image.TColor(int(color))).save(string)
     await interaction.response.send_message("Text: " + string, embed=open(string))
 
 @client.event

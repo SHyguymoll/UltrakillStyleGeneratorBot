c4c9a37d6e2c2ad8d984084a7d4339aa3bb0e089
SHyguymoll <shymollet@gmail.com>
2023-04-02 16:28:02 -0700

    bot no longer saves images locally

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 import os
 import generate_image
 import discord
+from io import BytesIO
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,14 +12,17 @@ tree = discord.app_commands.CommandTree(client)
 
 @tree.command(name = "generate_text", description = "Characters Supported: a-Z, 0-9, +, -, (, )\nSeparate strings with |") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction: discord.Interaction, name: str, string: str):
-    to_send = generate_image.full_image(string.split("|"))
-    act_name = name + ".png"
-    to_send.save(act_name)
+    make_img = generate_image.full_image(string.split("|"))
+
+    act_name = "out_discord/" + name + ".png"
     text = str("Text: " + name)
-    file = discord.File(fp=open(act_name, mode="rb"),filename=act_name)
+    to_use = BytesIO()
+    make_img.save(to_use, format='PNG')
+    to_use.seek(0)
+
+    file = discord.File(fp=to_use,filename=act_name)
     await interaction.response.send_message(content=text, file=file)
     file.close()
-    os.remove(act_name)
 
 @client.event
 async def on_ready():
@@ -41,4 +45,3 @@ def read_token() -> str:
         return f.readline()
 
 client.run(read_token())
-

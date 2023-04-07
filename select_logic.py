e7f2f3a076dcfcaf07581681bee50ad3304bbec3
SHyguymoll <shymollet@gmail.com>
2023-04-07 00:36:13 -0700

    classic if else chains for python3.9

--- select_logic.py
+++ select_logic.py
@@ -9,48 +9,47 @@ class TColor(Enum):
     GOLD = 5
 
 def pick_color(color: TColor):
-    match color:
-        case TColor.WHITE:
-            return (255, 255, 255, 255)
-        case TColor.ORANGE:
-            return (255, 170, 0, 255)
-        case TColor.GREEN:
-            return (22, 255, 29, 255)
-        case TColor.BLUE:
-            return (39, 255, 236, 255)
-        case TColor.RED:
-            return (255, 0, 0, 255)
-        case TColor.GOLD:
-            return (243, 211, 0)
+    if color == TColor.WHITE:
+        return (255, 255, 255, 255)
+    elif color == TColor.ORANGE:
+        return (255, 170, 0, 255)
+    elif color == TColor.GREEN:
+        return (22, 255, 29, 255)
+    elif color == TColor.BLUE:
+        return (39, 255, 236, 255)
+    elif color == TColor.RED:
+        return (255, 0, 0, 255)
+    elif color == TColor.GOLD:
+        return (243, 211, 0)
+    else: #another fallback because why not
+        return (255, 255, 255, 255)
 
 def select_color(val: int):
-    match val:
-        case 0:
-            return TColor.WHITE
-        case 1:
-            return TColor.ORANGE
-        case 2:
-            return TColor.GREEN
-        case 3:
-            return TColor.BLUE
-        case 4:
-            return TColor.RED
-        case 5:
-            return TColor.GOLD
-        case _: #invalid color index, fallback to white
-            return TColor.WHITE
+    if val == 0:
+        return TColor.WHITE
+    elif val == 1:
+        return TColor.ORANGE
+    elif val == 2:
+        return TColor.GREEN
+    elif val == 3:
+        return TColor.BLUE
+    elif val == 4:
+        return TColor.RED
+    elif val == 5:
+        return TColor.GOLD
+    else: #invalid color index, fallback to white
+        return TColor.WHITE
 
 def select_character(char_dict: dict, char: str, head: bool): #needs to return array
-    match char:
-        case "+":
-            return [char_dict["PLUS"][head], char_dict["SPACE"][head], char_dict["SPACE"][head], char_dict["SPACE"][head]]
-        case "-":
-            return [char_dict["MINUS"][head], char_dict["SPACE"][head], char_dict["SPACE"][head], char_dict["SPACE"][head]]
-        case "(":
-            return [char_dict["LEFT_BRACKET"][head]]
-        case ")":
-            return [char_dict["RIGHT_BRACKET"][head]]
-        case " ":
-            return [char_dict["SPACE"][head]]
-        case _:
-            return [char_dict[char.upper()][head]]
+    if char == "+":
+        return [char_dict["PLUS"][head], char_dict["SPACE"][head], char_dict["SPACE"][head], char_dict["SPACE"][head]]
+    elif char == "-":
+        return [char_dict["MINUS"][head], char_dict["SPACE"][head], char_dict["SPACE"][head], char_dict["SPACE"][head]]
+    elif char == "(":
+        return [char_dict["LEFT_BRACKET"][head]]
+    elif char == ")":
+        return [char_dict["RIGHT_BRACKET"][head]]
+    elif char == " ":
+        return [char_dict["SPACE"][head]]
+    else:
+        return [char_dict[char.upper()][head]]

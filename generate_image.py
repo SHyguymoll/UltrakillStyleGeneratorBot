85175ada200bc687a7841a74c4dfbabf1aa68f6b
SHyguymoll <shymollet@gmail.com>
2023-07-18 00:17:01 -0700

    I was wrong about the font thing

--- generate_image.py
+++ generate_image.py
@@ -3,10 +3,13 @@ from sys import argv
 from os import makedirs, path
 from select_logic import *
 
+STYLE_SIZE = 28
+HEADER_SIZE = 72
+
 def load_characters(lower_fontname: str, upper_fontname: str):
     global font_style, font_header
-    font_style = ImageFont.truetype(lower_fontname, 28)
-    font_header = ImageFont.truetype(upper_fontname, 72)
+    font_style = ImageFont.truetype(lower_fontname, STYLE_SIZE * 3)
+    font_header = ImageFont.truetype(upper_fontname, HEADER_SIZE * 3)
 
 def generate_character(character_mask: Image.Image, color: TColor) -> Image.Image:
     char = Image.new("RGBA", character_mask.size, pick_color(color))
@@ -15,22 +18,21 @@ def generate_character(character_mask: Image.Image, color: TColor) -> Image.Imag
 
 NO_COLOR = ImageColor.getrgb("#FFFFFF00")
 
-def write_text(text: str, is_header: bool, color: TColor) -> Image.Image:
+def write_text(text: str, is_header: bool, color: tuple[int]) -> Image.Image:
     if is_header:
         header_bbox = font_header.getbbox(text=text, anchor="ld")
         img_size = (header_bbox[2]-header_bbox[0], header_bbox[3]-header_bbox[1])
         img = Image.new('RGBA', img_size, NO_COLOR)
         draw = ImageDraw.Draw(img)
-        draw.text(xy=(0, img_size[1]), text=text, fill=pick_color(color), anchor="ld")
-        return img
+        draw.text(xy=(0, img_size[1]), text=text, font=font_header, fill=color, anchor="ld")
+        return img.resize((round(img.size[0] / 3), round(img.size[1] / 3)), Image.Resampling.BILINEAR)
     else:
         style_bbox = font_style.getbbox(text=text, anchor="ld")
         img_size = (style_bbox[2]-style_bbox[0], style_bbox[3]-style_bbox[1])
         img = Image.new('RGBA', img_size, NO_COLOR)
         draw = ImageDraw.Draw(img)
-        draw.fontmode = 1
-        draw.text(xy=(0, img_size[1]), text=text, fill=pick_color(color), anchor="ld")
-        return img
+        draw.text(xy=(0, img_size[1]), text=text, font=font_style, fill=color, anchor="ld")
+        return img.resize((round(img.size[0] / 3), round(img.size[1] / 3)), Image.Resampling.NEAREST)
 
 def generate_image(text_string: str, header: bool = False) -> Image.Image:
     interpret_string = text_string.split("_")
@@ -45,13 +47,13 @@ def generate_image(text_string: str, header: bool = False) -> Image.Image:
         if len(string) == 0: #after removal, the string might be empty, so return an empty image
             continue
         if header: #header, don't make any changes to the string
-            final = merge_hori(final, write_text(string, True, current_color))
+            final = merge_hori(final, write_text(string, True, pick_color(current_color)))
         else:
             if ind == 0 and string[0] == "+": #pluses come with spaces when at the start of the line
                 string = "+   " + string[1:]
             if ind == 0 and string[0] == "-": #same with minuses
                 string = "-   " + string[1:]
-            final = merge_hori(final, write_text(string, False, current_color))
+            final = merge_hori(final, write_text(string, False, pick_color(current_color)))
     return final
 
 def merge_hori(im1: Image.Image, im2: Image.Image) -> Image.Image:
